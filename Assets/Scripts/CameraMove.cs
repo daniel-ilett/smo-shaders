891b46dad61ab9a172334aeb092103eac85a0531
Daniel Ilett <danielilett@gmail.com>
2019-04-20 16:44:59 +0100

    Added CameraMove script.

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private const float moveSpeed = 7.5f;
+	private const float cameraSpeed = 3.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Vector2 rotation = Vector2.zero;
+
+	private void Awake()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	private void Update()
+	{
+		// Rotate the camera.
+		rotation += new Vector2(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
+		transform.eulerAngles = rotation * cameraSpeed;
+
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+		transform.Translate(move * moveSpeed * Time.deltaTime);
+	}
 }
