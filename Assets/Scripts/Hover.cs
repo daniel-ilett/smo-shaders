e227d6b9dfd6495e493832315a3844d2d767115d
Daniel Ilett <danielilett@gmail.com>
2019-05-14 16:58:37 +0100

    Added crystal models to example scene.
    Includes model of crystals, several materials, and Rotate and Hover scripts.

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,26 @@
+﻿/*  Hover an object relative to its parent over time.
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 hoverVector;
+
+    [SerializeField]
+    private Transform hoverTransform;
+
+    private Vector3 startPos;
+
+    private void Awake()
+    {
+        startPos = hoverTransform.localPosition;
+    }
+
+    private void Update()
+    {
+        hoverTransform.localPosition = startPos + hoverVector * Mathf.Sin(Time.time);
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,19 @@
+﻿/*  Rotate an object over time, relative to its parent.
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 eulerAngles;
+
+    [SerializeField]
+    private Transform rotationTransform;
+
+    private void Update()
+    {
+        rotationTransform.Rotate(eulerAngles * Time.deltaTime, Space.Self);
+    }
+}
