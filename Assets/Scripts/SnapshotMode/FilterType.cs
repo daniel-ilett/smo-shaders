465d158af2d2e4b12a377001e5d99ae5aacee81c
Daniel Ilett <danielilett@gmail.com>
2019-05-17 20:47:20 +0100

    Added Snapshot Mode files.
    A true Snapshot Mode will be added such that the user can drop a script on their main camera and they will be able to cycle through each effect and take a screenshot.

---
+++ Assets/Scripts/SnapshotMode/FilterType.cs
@@ -0,0 +1,19 @@
+﻿/*  This enum contains a list of all filter types that could be used in
+ *  Snapshot Mode.
+ */
+public enum FilterType
+{
+    None,
+    Greyscale,
+    Sepia,
+    FullBlur,
+    EdgeBlur,
+    Silhouette,
+    Outline,
+    Neon,
+    Bloom,
+    NES,
+    SNES,
+    GB,
+    Painting
+}
---
+++ Assets/Scripts/SnapshotMode/SnapshotMode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class SnapshotMode : MonoBehaviour
+{
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        
+    }
+}
