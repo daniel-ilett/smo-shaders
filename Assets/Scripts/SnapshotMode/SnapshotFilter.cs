6a6c20981ebd48ceb09042a7ee2ec6dd86dd7343
Daniel Ilett <danielilett@gmail.com>
2019-05-17 21:03:49 +0100

    Added skeleton structure for effects.

---
+++ Assets/Scripts/SnapshotMode/SnapshotFilter.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SnapshotFilter
+{
+    private Material mainMaterial;
+
+    public SnapshotFilter(Shader shader)
+    {
+        mainMaterial = new Material(shader);
+    }
+
+    public abstract void OnRenderImage(RenderTexture src, RenderTexture dst);
+}
+
+public class BaseFilter : SnapshotFilter
+{
+    public BaseFilter(Shader shader) : base(shader)
+    {
+
+    }
+
+    public override void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class BlurFilter : SnapshotFilter
+{
+    public BlurFilter(Shader shader) : base(shader)
+    {
+
+    }
+
+    public override void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class BloomFilter : SnapshotFilter
+{
+    public BloomFilter(Shader shader) : base(shader)
+    {
+
+    }
+
+    public override void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class NeonFilter : BloomFilter
+{
+    public NeonFilter(Shader shader) : base(shader)
+    {
+
+    }
+
+    public override void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class PixelFilter : SnapshotFilter
+{
+    public PixelFilter(Shader shader) : base(shader)
+    {
+
+    }
+
+    public override void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class CRTFilter : PixelFilter
+{
+    public CRTFilter(Shader shader) : base(shader)
+    {
+
+    }
+
+    public override void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/Scripts/SnapshotMode/SnapshotMode.cs
+++ Assets/Scripts/SnapshotMode/SnapshotMode.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 
 public class SnapshotMode : MonoBehaviour
 {
+    [SerializeField]
+    private bool useCanvas = true;
+
+    private void Awake()
+    {
+        // Add a canvas to show the current filter name and the controls.
+        if(useCanvas)
+        {
+
+        }
+    }
+
     private void OnRenderImage(RenderTexture src, RenderTexture dst)
     {
         
